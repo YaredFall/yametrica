189605950984ff0498c55181e4ef68cf594104c0
YaredFall <yaredfall@gmail.com>
2026-03-14 17:48:28 +0700

    fix(react): require clientID prop on YandexMetricaNoscript instead of value from non-reactive client

--- packages/react/src/yandex-metrica-noscript.tsx
+++ packages/react/src/yandex-metrica-noscript.tsx
@@ -1,18 +1,13 @@
 "use client";
 
 import { createYandexMetricaNoscript } from "@yandex-metrica/core";
-import { useYandexMetrica } from "./useYandexMetrica";
 
-export function YandexMetricaNoscript() {
-    const client = useYandexMetrica();
-
-    if (!client.clientID) return null;
-
+export function YandexMetricaNoscript({ clientID }: { clientID: string }) {
     return (
         <noscript
             // biome-ignore lint/security/noDangerouslySetInnerHtml: safe
             dangerouslySetInnerHTML={{
-                __html: createYandexMetricaNoscript(client.clientID),
+                __html: createYandexMetricaNoscript(clientID),
             }}
         />
     );
